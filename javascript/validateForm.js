2922585102b5019eea32b3192cca92955506c06f
sebasrodri237 <sebasplay2@hotmail.com>
2020-06-06 17:04:22 -0500

    Validate email, password and the checkbox is complete. The next is add some basic styles like background-colors and box-shadow

--- javascript/validateForm.js
+++ javascript/validateForm.js
@@ -19,26 +19,65 @@ class form{
             checkbox
         }
         this.validateName(e);
-        this.validateage(e);
-        // this.validateemail();
-        // this.validatepassword();
-        // this.validatecheckbox();
+        this.validateAge(e);
+        this.validateEmail(e);
+        this.validatePassword(e);
+        this.validateCheckbox(e);
     }
     validateName(e){
 
         if(this.data.name.value == ""){
 
-            alert('The name field cannot be empty');
+            alert('The name field cannot be empty.');
             e.preventDefault();
         }
     }
-    validateage(e){
+    validateAge(e){
 
         if(Number.isNaN(this.data.age.value) || this.data.age.value == "" ){
-            alert('The age field must be a integer number');
+            alert('The age field cannot be empty or must be a integer number.');
             e.preventDefault();
         }
     }
+    validateEmail(e){
+///////////////////// Regular expression to mail format is from: https://www.w3resource.com/javascript/form/email-validation.php //////////////////////////
+        let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        
+        if (!mailformat.test(this.data.email.value) || this.data.email.value == "" ){
+            alert('The email field cannot be empty or you have entered an invalid email address.');
+            e.preventDefault();
+        } 
+    }
+    validatePassword(e){
+
+        if(this.numberOfCharacters(this.data.password.value.length) || this.data.password.value == ""){
+
+            alert('The password field cannot be empty or the password have to contain  only 7-16 characters.');
+            e.preventDefault();
+        }
+    }
+    numberOfCharacters(passwordLength){
+
+        const MIN_CHARACTERES = 7;
+        const MAX_CHARACTERES = 16;
+
+        if(MIN_CHARACTERES > passwordLength || MAX_CHARACTERES < passwordLength){
+
+            return true;
+        }else{
+
+            return false;
+        }
+    }
+    validateCheckbox(e){
+
+        if(!this.data.checkbox.checked){
+
+            alert('You must accept the terms and conditions to continue.');
+            e.preventDefault();
+        }
+    }
+
 }
 
 myform.addEventListener('submit', validateForm);
